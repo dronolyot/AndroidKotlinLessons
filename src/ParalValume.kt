df8cd678d2104178cbd18d3accc642b36254e040
Oleg <dronolyot@yandex.ru>
2022-09-15 00:30:14 +0300

    First commit

---
+++ src/ParalValume.kt
@@ -0,0 +1,14 @@
+//Расчет объёма параллелепипеда
+fun main() {
+    //ширина
+    val width: Int = 5
+    //длина
+    val length: Int = 15
+    //высота
+    val height: Int = 10
+
+    //Результат объёма
+    val valResult: Int = width * length * height
+
+    println(valResult)
+}
